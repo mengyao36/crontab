7a7375a487d32bf99b451fb35fa244b34fa25e35
mengyao36 <mengyao.hu@stonybrook.edu>
2022-10-05 12:11:32 -0400

    edit python file

---
+++ code.py
@@ -0,0 +1,22 @@
+import os 
+import sys
+import time
+
+# get current working directory
+cwd = os.getcwd()
+
+# print cwd
+print(cwd)
+
+# create a new dictionary with dummy data
+data = {'id': 100, 'age': 25, 'gender': 'female'}
+
+# get the current time
+now = time.time()
+
+# save current time as a string
+nowStr = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(now))
+
+# create a new file in the current working directory
+with open(cwd + '/testFile_' + nowStr + '.txt', 'w') as f:
+    f.write(str(data))
---
+++ code.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Hello, we are going to now run the code.sh script"
+python code.py
